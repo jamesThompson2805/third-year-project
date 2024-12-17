29afc5e69c69c1b7f474562c281656da11a95636
jamesthompson <jamesthompson2805@gmail.com>
2024-12-17 17:17:21 +0000

    adjustments to build system

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell.override
+          {
+            # Override stdenv in order to change compiler:
+            # stdenv = pkgs.clangStdenv;
+          }
+          {
+            packages = with pkgs; [
+	      gcc
+              clang-tools
+              cmake
+              gtest
+	      boost
+            ] ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
+          };
+      });
+    };
+}
+
--- shell.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.clangStdenv.mkDerivation{
-    # nativeBuildInputs is usually what you want -- tools you need to run
-    name = "clang derivation";
-    nativeBuildInputs = with pkgs.buildPackages; [ gcc cmake boost gnuplot gtest];
-}
